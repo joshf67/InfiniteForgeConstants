56cd0921a269f5daab40ac75341d206e00add11b
Derrik <heyimderrik@gmail.com>
2022-09-17 00:10:59 -0400

    Created map class
    Eventually this should be a static type representation of the .mvar files.

---
+++ Map.cs
@@ -0,0 +1,9 @@
+namespace InfiniteForgeConstants;
+
+public class Map
+{
+    public MapId MapId { get; set; }
+    public List<GameObject> GameObjects { get; set; }
+    
+    //todo add the rest of the map specific data
+}
