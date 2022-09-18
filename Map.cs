d7257e9fa223284ed62f37768b8189509ff16be6
joshf67 <iownall99@live.co.uk>
2022-09-18 18:19:50 +0200

    Updated Map to allow null variables

--- Map.cs
+++ Map.cs
@@ -6,15 +6,14 @@ namespace InfiniteForgeConstants;
 
 public class Map
 {
-    public MapId MapId { get; set; }
+    public MapId? MapId;
     public bool? Decorators;
     public ScreenEffectId? ScreenEffectId;
     public MapOptions? Options;
     public AmbientSound? AmbientSound;
-    
-    public List<GameObject> GameObjects { get; set; }
+    public List<GameObject>? GameObjects;
 
-    public Map(MapId mapId = MapId.AQUARIUS, bool? decorators = null, ScreenEffectId? screenEffectId = null,
+    public Map(MapId mapId = MapSettings.MapId.AQUARIUS, bool? decorators = null, ScreenEffectId? screenEffectId = null,
         MapOptions? options = null, AmbientSound? ambientSound = null, List<GameObject>? gameObjects = null)
     {
         MapId = mapId;
