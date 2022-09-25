d3788fce7202c4dc5553176dcacb9168a3ceb6f4
joshf67 <iownall99@live.co.uk>
2022-09-25 05:25:40 +0100

    Added Node Graph Data
    Added a bunch of Enums that are selectable when using nodes inside Node Graph

---
+++ Node Graph/EquipmentTypes.cs
@@ -0,0 +1,13 @@
+﻿namespace InfiniteForgeConstants.NodeGraphSettings;
+
+public enum EquipmentTypes
+{
+    DROP_WALL = 0,
+    GRAPPLESHOT = 1,
+    REPULSOR = 2,
+    THREAT_SENSOR = 3,
+    THRUSTER = 4,
+    ACTIVE_CAMO = 5,
+    OVERSHIELD = 6,
+    CUSTOM_EQUIPMENT = 7
+}
