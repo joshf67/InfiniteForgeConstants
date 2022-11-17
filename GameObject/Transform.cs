24c9b3f000d114993cb062d7f1684ea04f0f8786
joshf67 <iownall99@live.co.uk>
2022-11-17 06:40:45 +0000

    Potentially fixed rotation issues
    Artifice and Oziwag gave updated transform maths.

--- GameObject/Transform.cs
+++ GameObject/Transform.cs
@@ -110,17 +110,21 @@ public class Transform
     /// <param name="forward"> The forward unit vector to use. </param>
     /// <param name="up"> The up unit vector to use. </param>
     /// <returns> The euler rotation in radians and degrees </returns>
+    /// <credits> Thanks to Artifice and Oziwag for this function. </credits>
     public static (Vector3 Radians, Vector3 Degrees) DirectionToEuler(Vector3 forward, Vector3 up)
     {
         if (forward == Vector3.Zero && up == Vector3.Zero || forward == up)
             return (Vector3.Zero, Vector3.Zero);
-            
-        var z = MathF.Acos(Vector3.Dot(Vector3.UnitX, Vector3.Normalize(forward with { Z = 0 }))) *
-                Vector3.Normalize(new Vector3(0, forward.Y, 0)).Y;
-
-        var y = MathF.Acos(Vector3.Dot(Vector3.UnitZ, forward)) - (MathF.PI / 2);
-
-        var x = MathF.Acos(Vector3.Dot(Vector3.UnitZ, Vector3.Cross(forward, up)));
+        
+        var rollvec = new Vector3(-forward.X,-forward.Y,forward.Z);
+        var z = MathF.Atan2(forward.Y, forward.X);
+        
+        var y = MathF.Atan2(forward.Z, MathF.Sqrt(forward.X * forward.X + forward.Y * forward.Y));
+ 
+        if (y > MathF.PI / 2) { y -= MathF.PI / 2; }
+        if (y < -MathF.PI / 2) { y += MathF.PI / 2; }
+        
+        var x = MathF.Atan2(Vector3.Dot(forward, Vector3.Cross(rollvec, up)), Vector3.Dot(rollvec, up));
 
         return (new Vector3(x, y, z), new Vector3(x, y, z) * (180 / MathF.PI));
     }
