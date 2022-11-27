476d76cee2fd85f2bfd0bf801d6f8f0ab213271f
joshf67 <iownall99@live.co.uk>
2022-11-27 17:49:11 +0000

    Update ObjectPropertiesOptions.cs
    Updated to include other UI options

--- Forge UI/Object Properties/ObjectPropertiesOptions.cs
+++ Forge UI/Object Properties/ObjectPropertiesOptions.cs
@@ -2,6 +2,45 @@ namespace InfiniteForgeConstants.Forge_UI.Object_Properties;
 
 public static class ObjectPropertiesOptions
 {
+    public static Dictionary<ObjectPropertyName, int> StaticOnly = new()
+    {
+        { ObjectPropertyName.GeneralDropdown, 0 },
+        { ObjectPropertyName.ObjectName, 1 },
+        { ObjectPropertyName.ObjectMode, 2 },
+        { ObjectPropertyName.SizeX, 3 },
+        { ObjectPropertyName.SizeY, 4 },
+        { ObjectPropertyName.SizeZ, 5 },
+        { ObjectPropertyName.Physics, 6 },
+        { ObjectPropertyName.TransformDropdown, 7 },
+        { ObjectPropertyName.PositionDropdown, 8 },
+        { ObjectPropertyName.Forward, 9 },
+        { ObjectPropertyName.Horizontal, 10 },
+        { ObjectPropertyName.Vertical, 11 },
+        { ObjectPropertyName.RotationDropdown, 12 },
+        { ObjectPropertyName.Yaw, 13 },
+        { ObjectPropertyName.Pitch, 14 },
+        { ObjectPropertyName.Roll, 15 },
+        { ObjectPropertyName.Reset, 16 }
+    };
+    
+    public static Dictionary<ObjectPropertyName, int> DynamicOnly = new()
+    {
+        { ObjectPropertyName.GeneralDropdown, 0 },
+        { ObjectPropertyName.ObjectName, 1 },
+        { ObjectPropertyName.ObjectMode, 2 },
+        { ObjectPropertyName.Physics, 3 },
+        { ObjectPropertyName.TransformDropdown, 4 },
+        { ObjectPropertyName.PositionDropdown, 5 },
+        { ObjectPropertyName.Forward, 6 },
+        { ObjectPropertyName.Horizontal, 7 },
+        { ObjectPropertyName.Vertical, 8 },
+        { ObjectPropertyName.RotationDropdown, 9 },
+        { ObjectPropertyName.Yaw, 10 },
+        { ObjectPropertyName.Pitch, 11 },
+        { ObjectPropertyName.Roll, 12 },
+        { ObjectPropertyName.Reset, 13 }
+    };
+    
     public static Dictionary<ObjectPropertyName, int> StaticByDefault = new()
     {
         { ObjectPropertyName.GeneralDropdown, 0 },
@@ -22,5 +61,24 @@ public static class ObjectPropertiesOptions
         { ObjectPropertyName.Pitch, 15 },
         { ObjectPropertyName.Roll, 16 },
         { ObjectPropertyName.Reset, 17 }
+    };
+    
+    public static Dictionary<ObjectPropertyName, int> DynamicByDefault = new()
+    {
+        { ObjectPropertyName.GeneralDropdown, 0 },
+        { ObjectPropertyName.ObjectName, 1 },
+        { ObjectPropertyName.ObjectModeDropdown, 2 },
+        { ObjectPropertyName.ObjectMode, 3 },
+        { ObjectPropertyName.Physics, 4 },
+        { ObjectPropertyName.TransformDropdown, 5 },
+        { ObjectPropertyName.PositionDropdown, 6 },
+        { ObjectPropertyName.Forward, 7 },
+        { ObjectPropertyName.Horizontal, 8 },
+        { ObjectPropertyName.Vertical, 9 },
+        { ObjectPropertyName.RotationDropdown, 10 },
+        { ObjectPropertyName.Yaw, 11 },
+        { ObjectPropertyName.Pitch, 12 },
+        { ObjectPropertyName.Roll, 13 },
+        { ObjectPropertyName.Reset, 14 }
     };
 }
